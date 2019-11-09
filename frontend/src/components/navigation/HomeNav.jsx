09ff94a737ae17db18b48e3b8a4c1ac5534f3db2
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2019-11-09 23:37:49 +0100

    ch(tidy-up-homepage): add functionalities (#20)
    - add functionality to mobile navbar
    - add more styles to navbar
    
    [Delivers tidyUpHomepage]

--- frontend/src/components/navigation/HomeNav.jsx
+++ frontend/src/components/navigation/HomeNav.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import TopNav from './TopNav';
+import cancelBtn from '../../assets/cancel.svg';
 
-const HomeNav = () => (
-  <div>
-    <nav className="navbar">
-      <div className="navbar__wrapper">
-        <div className="home__nav">
-          <TopNav />
-          <div className="home__nav--bottom-nav">
-            <div className="nav-options">
-              <Link to="/" className="link">
-                Aout Oasis
-              </Link>
-              <Link to="/" className="link">
-                Faculties
-              </Link>
-              <Link to="/" className="link">
-                Staff
-              </Link>
-              <Link to="/" className="link">
-                Post Graduate
-              </Link>
-              <Link to="/login" className="link">
-                Login
-              </Link>
+const initialState = {
+  showBottomNav: false,
+};
+
+const HomeNav = () => {
+  const [state, setState] = useState(initialState);
+
+  const toggleNavbar = () => {
+    setState({ ...state, showBottomNav: !state.showBottomNav });
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__wrapper">
+          <div className="home__nav">
+            <TopNav onMenuClick={toggleNavbar} />
+            <div
+              className={`home__nav--bottom-nav ${
+                state.showBottomNav ? 'home__nav--bottom-nav--displayed' : ''
+              }`}
+            >
+              <div className="nav-options">
+                <div className="cancel">
+                  <img
+                    src={cancelBtn}
+                    alt="cancel icon"
+                    onClick={toggleNavbar}
+                  />
+                </div>
+                <Link to="/" className="link">
+                  Aout Oasis
+                </Link>
+                <Link to="/" className="link">
+                  Faculties
+                </Link>
+                <Link to="/" className="link">
+                  Staff
+                </Link>
+                <Link to="/" className="link">
+                  Post Graduate
+                </Link>
+                <Link to="/login" className="link">
+                  Login
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-  </div>
-);
+      </nav>
+    </div>
+  );
+};
 
 export default HomeNav;
--- frontend/src/components/navigation/TopNav.jsx
+++ frontend/src/components/navigation/TopNav.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const TopNav = () => (
+const TopNav = (props) => (
   <div className="home__nav--top-nav">
     <p className="logo">Oasis College</p>
-    <div className="menu">
+    <div className="menu" onClick={props.onMenuClick}>
       <span className="menu__item"></span>
       <span className="menu__item"></span>
       <span className="menu__item"></span>
